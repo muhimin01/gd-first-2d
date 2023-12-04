9793423a8914647eb56d12233f3c139e45a984ae
muhimin01 <62156192+muhimin01@users.noreply.github.com>
2023-12-04 18:50:51 +0800

    Create player scene

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var speed = 400	# How fast the player will move
+var screen_size			# Size of the game windows
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
