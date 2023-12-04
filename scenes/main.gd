ec240efe86ccba77d596f830c34c68ca05871667
muhimin01 <62156192+muhimin01@users.noreply.github.com>
2023-12-05 03:00:04 +0800

    Add UI

--- scenes/main.gd
+++ scenes/main.gd
@@ -5,7 +5,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
+	pass # Replace with function body
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,11 +14,15 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene
@@ -47,6 +51,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
