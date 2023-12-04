18a58728886cddc93dd98b2a5d2cfe47496b3089
muhimin01 <62156192+muhimin01@users.noreply.github.com>
2023-12-04 19:18:20 +0800

    Add movement to player

--- player.gd
+++ player.gd
@@ -1,13 +1,39 @@
 extends Area2D
 
-@export var speed = 400	# How fast the player will move
-var screen_size			# Size of the game windows
+@export var speed = 400 # How fast the player will move
+var screen_size # Size of the game windows
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO # The player's movement vector
+	
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	else:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
